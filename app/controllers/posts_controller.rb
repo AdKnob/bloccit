e0a4528815fc203fd4cb1dd92a9d8b1d08c120e4
Adrian Knoblauch <adrianknoblauch@me.com>
2016-12-10 21:22:35 +0100

    Checkpoint 39: Migrations and Associations completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :require_sign_in, except: :show
+
   def show
     @post = Post.find(params[:id])
   end
@@ -12,11 +14,9 @@ class PostsController < ApplicationController
 
 
    def create
-      @post = Post.new
-      @post.title = params[:post][:title]
-      @post.body = params[:post][:body]
-      @topic = Topic.find(params[:topic_id])
-      @post.topic = @topic
+     @topic = Topic.find(params[:topic_id])
+     @post = @topic.posts.build(post_params)
+     @post.user = current_user
 
       if @post.save
         flash[:notice] = "Post was saved successfully."
@@ -34,9 +34,8 @@ class PostsController < ApplicationController
 
 
   def update
-     @post = Post.find(params[:id])
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
+    @post = Post.find(params[:id])
+    @post.assign_attributes(post_params)
 
      if @post.save
        flash[:notice] = "Post was updated successfully."
@@ -60,5 +59,9 @@ class PostsController < ApplicationController
      end
    end
 
+   private
+     def post_params
+       params.require(:post).permit(:title, :body)
+     end
 
 end
